f03e2412ac56ff67df061f9f496bddcd8fc1efef
yxb <964854596@qq.com>
2021-09-26 14:50:46 +0800

    springdemo-2

--- src/main/java/com/yxb/demo/Hello.java
+++ src/main/java/com/yxb/demo/Hello.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Hello {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-        return "Hello SpringBoot! ";
+        return "Hello SpringBoot!+++++ ";
     }
 }
